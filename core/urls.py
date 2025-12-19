20c65bcfe87045ed043febbdda6e4c408dcebbfa
Sohibnut <mr.nut.sohib@gmail.com>
2025-12-19 16:40:58 +0500

    without f in url

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('f/', include('fapp.urls')),
+    path('', include('fapp.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
